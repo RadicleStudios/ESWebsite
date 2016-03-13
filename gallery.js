3bc64223b024a1555c148fa1e409d9f4e98cccd5
Raphael Schindler <raphael@schindler.io>
2016-03-12 20:45:10 -0800

    Implement a new gallery; add updated GW, Bianca, and Vanilla assets; transition towards a new style

--- gallery.js
+++ gallery.js
@@ -1,73 +1,73 @@
 ---
 ---
-// The purpose is to 'load' image elements contained within horizontally-scrolling container DIVs; where 'loading' entails the following: as soon as the images are scrolled into view, their `src` attribute is replaced by their `data-src` attribute, with the intended effect that the quick-loading `src` image assets be replaced by the slower-loading assets referred by `data-src`, but only when needed, ie when scrolled into view.
-
 "use strict";
+
+function sleep (duration) {
+  var now = new Date().getTime();
+  while (new Date().getTime() < now + duration) { /* Do nothing. */ } 
+}
 
 window.onload = function () {
 
   // Loop through the gallery containers.
-  var galleryContainerNodeList = document.querySelectorAll('.GalleryContainer'); // TODO: Replace with something that is more widely supported.
+  var galleryContainerNodeList = document.querySelectorAll('.gallery-container'); // TODO: Replace with something that is more widely supported.
   var galleryContainerElements = Array.prototype.slice.call(galleryContainerNodeList); // Get a true Array.
   galleryContainerElements.forEach(function(containerElement) {
 
-    // Get the image view element.
-    var imageViewElement = containerElement.querySelector('.ImageView');
-    // TODO: Do something if there is no image view element.
-    imageViewElement.onclick = function() {
-      imageViewElement.style.visibility = "hidden";
-      imageViewElement.src = null;
-    }
-
-    // Loop through the contained images (the thumbnails). NB: The class must be `Thumbnail`.
-    var galleryThumbnailNodeList = containerElement.querySelectorAll('img.Thumbnail'); // TODO: Replace with something that is more widely supported.
+    // Loop through the contained images (i.e. the thumbnails).
+    // NB: The class must be `thumbnail`.
+    var galleryThumbnailNodeList = containerElement.querySelectorAll('img.thumbnail'); // TODO: Replace with something that is more widely supported.
     var galleryThumbnailImageElements = Array.prototype.slice.call(galleryThumbnailNodeList); // Get a true Array.
     galleryThumbnailImageElements.forEach(function(thumbnailElement) {
 
       // Install the onclick action.
-      thumbnailElement.onclick = function() {
-        console.log(imageViewElement.src);
-        imageViewElement.src = thumbnailElement.getAttribute('data-src');
-        imageViewElement.style.visibility = "visible";
+      thumbnailElement.onclick = function () {
+
+        // Create the 'image view' element.
+        var imageViewElement = document.createElement("img"); // Create a new image element.
+        imageViewElement.src = thumbnailElement.getAttribute('data-src'); // Set its source to be the value of the `data-src` attribute.
+
+        // Produce an overlay.
+        var overlay = document.createElement("div");
+        overlay.style.cssText = "width: 100%; height: 100%; position: fixed; top: 0; left: 0; z-index: 1; opacity: 0.6; background-color: #000;"
+        overlay.hidden = true;
+        overlay.onclick = function () {
+          imageViewElement.parentNode.removeChild(imageViewElement);
+          overlay.hidden = true;
+        }
+        document.body.insertBefore(overlay, document.body.firstChild);
+
+        // Reuse the overlay's onclick on the image view.
+        imageViewElement.onclick = overlay.onclick;
+
+        // Continue once the image has loaded...
+        imageViewElement.onload = function () {
+
+          // Calculate the image view's position.
+          var thumbnailHeight = thumbnailElement.getBoundingClientRect().bottom - thumbnailElement.getBoundingClientRect().top;
+          var thumbnailVerticalOffset = thumbnailElement.getBoundingClientRect().top - containerElement.getBoundingClientRect().top;
+          var imageHeight = imageViewElement.naturalHeight;
+          var imageVerticalOffset = thumbnailVerticalOffset - (imageHeight - thumbnailHeight) / 2;
+
+          // Sanitize it.
+          var containerHeight = containerElement.getBoundingClientRect().bottom - containerElement.getBoundingClientRect().top;
+          if (imageVerticalOffset + imageHeight > containerHeight) imageVerticalOffset = containerHeight - imageHeight;
+          if (imageVerticalOffset < 0) imageVerticalOffset = 0;
+          
+          // Insert the image view into the DOM and position it.
+          containerElement.insertBefore(imageViewElement, containerElement.firstChild); // Insert it as the first child of the container.
+          imageViewElement.style.zIndex = 2;
+          imageViewElement.style.position = "absolute";
+          containerElement.style.position = "relative";
+          imageViewElement.style.top = imageVerticalOffset + "px";
+
+          // Activate the overlay.
+          overlay.hidden = false;
+        }
+
       }
     });
 
   });
 
-    // // Loop through the horizontally-scrolling containers.
-    // var containerNodeList = document.querySelectorAll('.ManagedImageContainer');
-    // for (var i = 0; i < containerNodeList.length; i++) {
-    //
-    //     var containerElement = containerNodeList[i];
-    //
-    //     // Get the 'managed' image elments within this container.
-    //     // NB: We depend upon the array variable's being captured inside the upcoming closure.
-    //     var managedImageElementsNodeList = containerElement.querySelectorAll('img[data-src]');
-    //     var managedImageElements = Array.prototype.slice.call(managedImageElementsNodeList);
-    //
-    //     // Declare the 'loading' function.
-    //     var manageImages = function() {
-    //
-    //         var containerRect = containerElement.getBoundingClientRect();
-    //
-    //         // Loop through the 'managed' images.
-    //         for (var j = 0; j < managedImageElements.length; j++) {
-    //
-    //             // If the image should be 'loaded', 'load' it (and correspondingly remove the element from `managedImageElements`.
-    //             var imageElement = managedImageElements[j];
-    //             var imageRect = imageElement.getBoundingClientRect();
-    //             if (imageRect.left < containerRect.right && imageRect.right > containerRect.left) {
-    //                 imageElement.src = imageElement.getAttribute('data-src');
-    //                 managedImageElements.splice(j, 1); j--; // Remove the element from our array, and adjust the loop counter.
-    //             }
-    //         }
-    //
-    //     };
-    //
-    //     // Call the function now, to 'load' any visible images.
-    //     manageImages();
-    //
-    //     // Have the function called whenever the container scrolls.
-    //     containerElement.onscroll = manageImages;
-    // }
 };
